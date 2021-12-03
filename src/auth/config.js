6f5238949c0da09ee40cc158f19369973daeb48b
Alanis Yates <alanis.yates@thesneakerssociety.com>
2021-12-03 18:13:14 -0500

    adding in email and google auth

---
+++ src/auth/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD0hRrAj70bXNU1FC_99600jXynzPuID6E",
+  authDomain: "sneaker-society.firebaseapp.com",
+  projectId: "sneaker-society",
+  storageBucket: "sneaker-society.appspot.com",
+  messagingSenderId: "903067817008",
+  appId: "1:903067817008:web:2d69e610120d76f4ca89f5",
+  measurementId: "G-5DF9HGJTMY",
+};
+
+export default firebaseConfig;
--- src/components/Pages/Login/LoginPage.js
+++ src/components/Pages/Login/LoginPage.js
@@ -1,9 +1,49 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import {
+  auth,
+  logout,
+  signIn,
+  signInWithGoogle,
+  whoIsIn,
+  // signInWithGoogle,
+} from "../../../auth/firebase";
+import "./LoginPage.css";
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [user, loading, error] = useAuthState(auth);
+  // const history = useHistory();
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="login">
+      <div className="login__container">
+        <input
+          type="text"
+          className="login__textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail Address"
+        />
+        <input
+          type="password"
+          className="login__textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button className="login__btn" onClick={() => signIn(email, password)}>
+          Login
+        </button>
+        <button className="login__btn" onClick={() => signInWithGoogle()}>
+          Login Google
+        </button>
+        <button className="login__btn" onClick={() => whoIsIn()}>
+          Curernt User
+        </button>
+        <button className="login__btn" onClick={() => logout()}>
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
