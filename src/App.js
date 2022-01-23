f2ed2029b6a1d11cb648a1ff944f887bf4936927
Alanis Yates <yatesalanis@gmail.com>
2022-01-22 22:37:04 -0500

    connecting front end to back end

--- src/App.js
+++ src/App.js
@@ -6,11 +6,27 @@ import Explore from "./components/Pages/Explore";
 import Groups from "./components/Pages/Groups";
 import Profile from "./components/Pages/Profile";
 import LoginPage from "./components/Pages/Login";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_API_URL).then((response) => {
+      setUserData(response.data);
+    });
+  }, []);
+  const [userData, setUserData] = useState([]);
+ 
   return (
     <Router>
       <div className="App">
+        <button
+          onClick={() => {
+            console.log(userData);
+          }}
+        >
+          Hello
+        </button>
         <ul>
           <li>
             <Link to="/mysociety">My Society</Link>
