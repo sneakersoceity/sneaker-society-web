32523f752b5f3f9e06e2ee2e4a0b7f86680bd208
Alanis Yates <yatesalanis@gmail.com>
2022-06-24 22:19:36 -0400

    test remove inage

--- src/components/Pages/Members/Forms/PhotoUploadForm.js
+++ src/components/Pages/Members/Forms/PhotoUploadForm.js
@@ -1,38 +1,9 @@
-import { Button, Stack, TextField, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import React, { useState, useEffect } from "react";
-import styled from "@emotion/styled";
 import { useFormikContext } from "formik";
-import { Box } from "@mui/system";
 import { useDropzone } from "react-dropzone";
-
-const Input = styled("input")({
-  display: "none",
-});
-
-const thumbsContainer = {
-  display: "flex",
-  flexDirection: "row",
-  flexWrap: "wrap",
-  marginTop: 16,
-};
-
-const thumb = {
-  display: "inline-flex",
-  borderRadius: 2,
-  border: "1px solid #eaeaea",
-  marginBottom: 8,
-  marginRight: 8,
-  width: 100,
-  height: 100,
-  padding: 4,
-  boxSizing: "border-box",
-};
-
-const thumbInner = {
-  display: "flex",
-  minWidth: 0,
-  overflow: "hidden",
-};
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 const img = {
   display: "block",
@@ -41,9 +12,26 @@ const img = {
 };
 
 export default function PhotoUploadForm(props) {
-  const [files, setFiles] = useState([]);
   const formikProps = useFormikContext();
+  useEffect(() => {
+    if (formikProps.values.file?.length > 0) {
+      setFiles(
+        formikProps.values.file.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    }
+  }, []);
+  const [files, setFiles] = useState([]);
 
+  const remove = (file) => {
+    const newFiles = [...files];
+    newFiles.splice(newFiles.indexOf(file), 1);
+    formikProps.setFieldValue("file", newFiles);
+    setFiles(newFiles);
+  };
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
       "image/*": [],
@@ -60,19 +48,20 @@ export default function PhotoUploadForm(props) {
     },
   });
 
-  const thumbs = files.map((file) => (
-    <div style={thumb} key={file.name}>
-      <div style={thumbInner}>
-        <img
-          src={file.preview}
-          style={img}
-          // Revoke data uri after image is loaded
-          onLoad={() => {
-            URL.revokeObjectURL(file.preview);
-          }}
-        />
-      </div>
-    </div>
+  const thumbs = files.map((file, index) => (
+    <ImageListItem key={index}>
+      <img
+        src={file.preview}
+        style={img}
+        // Revoke data uri after image is loaded
+        onLoad={() => {
+          URL.revokeObjectURL(file.preview);
+        }}
+        onClick={() => {
+          remove(file);
+        }}
+      />
+    </ImageListItem>
   ));
 
   useEffect(() => {
@@ -86,29 +75,10 @@ export default function PhotoUploadForm(props) {
         <input {...getInputProps()} />
         <p>Drag 'n' drop some files here, or click to select files</p>
       </div>
-      <aside style={thumbsContainer}>{thumbs}</aside>
-      {/* <h1>Photo Intake Form</h1>
-      <label htmlFor="contained-button-file">
-        <Input
-          accept="image/*"
-          id="contained-button-file"
-          multiple
-          type="file"
-          onChange={(e) => {
-            formikProps.setFieldValue("file", e.target.files);
-          }}
-        />
-        <Button variant="contained" component="span">
-          Upload
-        </Button>
-      </label> */}
-      {/* <Button
-        onClick={() => {
-          console.log(formikProps.values);
-        }}
-      >
-        Log Values
-      </Button> */}
+      <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+        {thumbs}
+      </ImageList>
+
       <Typography variant="p" pt={3}>
         Uploaded Files:{" "}
         {formikProps.values.file?.length ? formikProps.values.file.length : 0}
